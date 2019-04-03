a2e14e7f82329dca332804eaff404231ba2f4b49
Ralf Boernemeier <ralf.boernemeier@ralfb-web.de>
2019-04-03 13:27:58 +0200

    Delete ControllerInjectable
    Delete ControllerInjectable Interface and the MainController partr in
    the ControllerFactory class. In the MVC Pattern data has to be exchanged
    using the model and not from controller to a second controller.

--- MVC-Example/src/de/ralfb_web/utils/ControllerFactory.java
+++ MVC-Example/src/de/ralfb_web/utils/ControllerFactory.java
@@ -2,7 +2,6 @@ package de.ralfb_web.utils;
 
 import de.ralfb_web.model.Model;
 import de.ralfb_web.services.DAOService;
-import de.ralfb_web.ui.MainController;
 
 public class ControllerFactory {
 
@@ -17,7 +16,7 @@ public class ControllerFactory {
 	 * @param            <T> Generic
 	 * @return ControllerInstance
 	 */
-	public static <T> T controllerForClass(Class<T> clazz, Model model, DAOService dao, MainController controller) {
+	public static <T> T controllerForClass(Class<T> clazz, Model model, DAOService dao) {
 		try {
 			// controllerInstance#someArbitraryMethodCall if necessary
 			// will be triggered *before* the initialize method!
@@ -26,11 +25,6 @@ public class ControllerFactory {
 			if (ModelInjectable.class.isAssignableFrom(clazz)) {
 				ModelInjectable c = (ModelInjectable) controllerInstance;
 				c.setModel(model);
-			}
-
-			if (ControllerInjectable.class.isAssignableFrom(clazz)) {
-				ControllerInjectable c = (ControllerInjectable) controllerInstance;
-				c.setController(controller);
 			}
 
 			if (DAOServiceInjectable.class.isAssignableFrom(clazz)) {
@@ -46,11 +40,7 @@ public class ControllerFactory {
 	}
 
 	public static <T> T controllerForClass(Class<T> clazz, Model model) {
-		return controllerForClass(clazz, model, null, null);
-	}
-	
-	public static <T> T controllerForClass(Class<T> clazz, Model model, DAOService dao) {
-		return controllerForClass(clazz, model, dao, null);
+		return controllerForClass(clazz, model, null);
 	}
 
 }
--- MVC-Example/src/de/ralfb_web/utils/ControllerInjectable.java
+++
@@ -1,7 +0,0 @@
-package de.ralfb_web.utils;
-
-import de.ralfb_web.ui.MainController;
-
-public interface ControllerInjectable {
-	void setController(MainController controller);
-}
